0ffb90143b89982392cf38fec2bfce90fdac41d7
denis_levchenko <tranzi90@gmail.com>
2023-01-16 22:33:01 +0300

    add IndexController.php

---
+++ app/Http/Controllers/Post/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        $posts = Post::all();
+
+        foreach ($posts as $post)
+            dump($post->title);
+
+    }
+}
