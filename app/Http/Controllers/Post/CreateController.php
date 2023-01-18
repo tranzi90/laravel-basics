9ead1cedbd3c9fe051f7adba7431c441d15d208d
denis_levchenko <tranzi90@gmail.com>
2023-01-18 21:34:15 +0300

    refactor controllers structure

--- app/Http/Controllers/Post/CreateController.php
+++ app/Http/Controllers/Post/CreateController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Post;
 
+use App\Http\Controllers\Controller;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class CreateController extends Controller
 {
